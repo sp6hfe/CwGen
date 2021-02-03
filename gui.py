d9e46a6a16340a179cd1c50a33f0c196570aae2a
Grzegorz Kaczmarek <sp6hfe@gmail.com>
2021-02-03 01:34:41 +0100

    GUI layout update + config variables added.

--- gui.py
+++ gui.py
@@ -2,15 +2,29 @@ import PySimpleGUI as sg
 import cwgen
 import os
 
+# button config
+FILE_BROWSE_KEY = '-ADD FILE-'
+FILE_REMOVE_KEY = '-REMOVE FILE-'
+
+# input config
+FILE_PATH_INPUT_KEY = '_file_to_add_'
+
+# listbox config
+FILES_LISTBOX_WIDTH = 40
+FILES_LISTBOX_HEIGHT = 10
+FILE_NAMES_KEY = '-FILE NAMES-'
+FILE_PATHS_KEY = '-FILE PATHS-'
+
 sg.theme('Default1')
 
+files_operation = [sg.Input(enable_events=True, visible=False, key=FILE_PATH_INPUT_KEY),
+                       sg.FileBrowse(button_text="Add new", file_types=(("ALL Files", "*.*"),("CWOPS sessions", "*.cwo")), target=FILE_PATH_INPUT_KEY, key=FILE_BROWSE_KEY),
+                       sg.Button(button_text="Remove selected", key=FILE_REMOVE_KEY)]
+
+files_data = [sg.Listbox(values=[], select_mode="LISTBOX_SELECT_MODE_MULTIPLE", disabled=True , size=(FILES_LISTBOX_WIDTH, FILES_LISTBOX_HEIGHT), key=(FILE_NAMES_KEY)), sg.Listbox(values=[], visible=False, key='-FILE PATHS-')]
+
 # Left column - input data
-left_col = [
-            [sg.Input(enable_events=True, visible=False, key='_file_to_add_'),
-             sg.FileBrowse(button_text="Add dictionary", file_types=(("ALL Files", "*.*"),("CWOPS sessions", "*.cwo")), target='_file_to_add_', key='-ADD FILE-'),
-             sg.Button(button_text="Remove selected", key='-REMOVE FILE-')],
-            [sg.Frame('Dictionaries', [[sg.Listbox(values=[], select_mode="LISTBOX_SELECT_MODE_MULTIPLE", disabled=True , size=(50, 10), key=('-FILE NAMES-'))], [sg.Listbox(values=[], visible=False, key='-FILE PATHS-')]])]
-           ]
+left_col = [[sg.Frame('Dictionaries', [files_operation, files_data])]]
 
 # Right column - config based on data
 right_col = [[sg.T('TEST')]]
@@ -29,33 +43,33 @@ while True:
         break
 
     # Add a dictionary to the list
-    if event == '_file_to_add_':
-        file_path, file_name = os.path.split(values['_file_to_add_'])
+    if event == FILE_PATH_INPUT_KEY:
+        file_path, file_name = os.path.split(values[FILE_PATH_INPUT_KEY])
         # serve only newly populated items
         if len(file_name) > 0:
-            current_file_names = window['-FILE NAMES-'].get_list_values()
-            current_file_paths = window['-FILE PATHS-'].get_list_values()
+            current_file_names = window[FILE_NAMES_KEY].get_list_values()
+            current_file_paths = window[FILE_PATHS_KEY].get_list_values()
             if file_name not in current_file_names:
                 current_file_names.append(file_name)
                 current_file_paths.append(file_path)
-                window['-FILE NAMES-'].update(disabled=False, values=current_file_names)
-                window['-FILE PATHS-'].update(disabled=False, values=current_file_paths)
+                window[FILE_NAMES_KEY].update(disabled=False, values=current_file_names)
+                window[FILE_PATHS_KEY].update(disabled=False, values=current_file_paths)
         # clear file path storage to properly handle CANCEL situation
-        window['_file_to_add_'].update(value="")
+        window[FILE_PATH_INPUT_KEY].update(value="")
 
     # remove dictionary from the list
-    if event == '-REMOVE FILE-':
-        selected_files_indexes = window['-FILE NAMES-'].get_indexes()
+    if event == FILE_REMOVE_KEY:
+        selected_files_indexes = window[FILE_NAMES_KEY].get_indexes()
         if len(selected_files_indexes) > 0:
             updated_file_names = []
             updated_file_paths = []
-            current_file_names = window['-FILE NAMES-'].get_list_values()
+            current_file_names = window[FILE_NAMES_KEY].get_list_values()
             for index, file_name in enumerate(current_file_names):
                 if index not in selected_files_indexes:
                     updated_file_names.append(current_file_names[index]) 
                     updated_file_paths.append(current_file_paths[index]) 
-            window['-FILE NAMES-'].update(disabled=False, values=updated_file_names)
-            window['-FILE PATHS-'].update(disabled=False, values=current_file_paths)
+            window[FILE_NAMES_KEY].update(disabled=False, values=updated_file_names)
+            window[FILE_PATHS_KEY].update(disabled=False, values=current_file_paths)
 
 # Finish up by removing from the screen
 window.close()
