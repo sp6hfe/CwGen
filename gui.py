fa199c790ceb0b5188a076d111f28e7cf1ce9745
Grzegorz Kaczmarek <sp6hfe@gmail.com>
2021-02-04 01:20:51 +0100

    Added words length sliders being updated on dictionary list change - if needed.

--- gui.py
+++ gui.py
@@ -46,6 +46,7 @@ def handle_dictionary_add(window, values):
                         window[FILE_NAMES_KEY].update(values=current_file_names)
                         window[FILE_PATHS_KEY].update(values=current_file_paths)
                         window[FILE_STATS_KEY].update(values=current_file_stats)
+                        update_words_length_sliders_config(window, values, calculate_words_length_range(current_file_stats))
         # clear file path storage to properly handle CANCEL situation
         window[FILE_PATH_INPUT_KEY].update(value="")
 
@@ -66,17 +67,55 @@ def handle_dictionary_delete(window, values):
             window[FILE_NAMES_KEY].update(values=updated_file_names)
             window[FILE_PATHS_KEY].update(values=updated_file_paths)
             window[FILE_STATS_KEY].update(values=updated_file_stats)
+            update_words_length_sliders_config(window, values, calculate_words_length_range(updated_file_stats))
 
 def handle_words_length_sliders(event, values):
-    min_val = values[LETTERS_MIN_KEY]
-    max_val = values[LETTERS_MAX_KEY]
+    slider_min_val = values[LETTERS_MIN_KEY]
+    slider_max_val = values[LETTERS_MAX_KEY]
 
     if event == LETTERS_MIN_KEY:
-        if min_val > max_val:
-            window[LETTERS_MAX_KEY].update(value=min_val)
+        if slider_min_val > slider_max_val:
+            window[LETTERS_MAX_KEY].update(value=slider_min_val)
     if event == LETTERS_MAX_KEY:
-        if max_val < min_val:
-            window[LETTERS_MIN_KEY].update(value=max_val)
+        if slider_max_val < slider_min_val:
+            window[LETTERS_MIN_KEY].update(value=slider_max_val)
+
+def calculate_words_length_range(words_stat):
+    letters_min_count = 1000
+    letters_max_count = 0
+
+    for words_count, letters_min, letters_max, stat in words_stat:
+        if letters_min_count > letters_min:
+            letters_min_count = letters_min
+        if letters_max_count < letters_max:
+            letters_max_count = letters_max
+
+    return (letters_min_count, letters_max_count)
+
+def update_words_length_sliders_config(window, values, new_range):
+    current_range_min, current_range_max = window[LETTERS_MIN_KEY].Range
+    current_min_val = int(values[LETTERS_MIN_KEY])
+    current_max_val = int(values[LETTERS_MAX_KEY])
+
+    new_range_min, new_range_max = new_range
+    new_min_val = current_min_val
+    new_max_val = current_max_val
+
+    # range min value may affect sliders position
+    if new_range_min > current_range_min:
+        if new_range_min > current_min_val:
+            new_min_val = new_range_min
+        if new_min_val > current_max_val:
+            new_max_val = new_min_val
+    # range max value may affect sliders position
+    if new_range_max < current_range_max:
+        if new_range_max < current_max_val:
+            new_max_val = new_range_max
+        if new_max_val < current_min_val:
+            new_min_val = new_max_val
+    
+    window[LETTERS_MIN_KEY].update(range=new_range, value=new_min_val)
+    window[LETTERS_MAX_KEY].update(range=new_range, value=new_max_val)
 
 # window theme
 sg.theme('Default1')
@@ -90,9 +129,9 @@ files_data = [sg.Listbox(values=[], select_mode="LISTBOX_SELECT_MODE_SINGLE", si
               sg.Listbox(values=[], visible=False, key='-FILE PATHS-'),
               sg.Listbox(values=[], visible=False, key='-FILE STATS-')]
 
-letters_min = [sg.Text("MIN", size=(4, 1)), sg.Slider(size=(H_SLIDER_WIDTH, H_SLIDER_HEIGHT), orientation='h', enable_events=True, key=LETTERS_MIN_KEY)]
+letters_min = [sg.Text("MIN", size=(4, 1)), sg.Slider(range=(0, 0), size=(H_SLIDER_WIDTH, H_SLIDER_HEIGHT), orientation='h', enable_events=True, key=LETTERS_MIN_KEY)]
 
-letters_max = [sg.Text("MAX", size=(4, 1)), sg.Slider(size=(H_SLIDER_WIDTH, H_SLIDER_HEIGHT), orientation='h', enable_events=True, key=LETTERS_MAX_KEY)]
+letters_max = [sg.Text("MAX", size=(4, 1)), sg.Slider(range=(0, 0), size=(H_SLIDER_WIDTH, H_SLIDER_HEIGHT), orientation='h', enable_events=True, key=LETTERS_MAX_KEY)]
 
 left_col = [[sg.Frame('Dictionaries', [files_operation, files_data])],
             [sg.Frame('Words letters count', [letters_min, letters_max])]]
