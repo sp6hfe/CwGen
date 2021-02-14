14ba2b19719498d5c16ff73efb3f0e2d12e7c8fd
Grzegorz Kaczmarek <sp6hfe@gmail.com>
2021-02-14 02:15:23 +0100

    Code splitted into files.

--- cwgen.py
+++ cwgen.py
@@ -1,8 +1,7 @@
-import hashlib
+import ebook2cw as e2cw
+import helpers
 import os
-import platform
 import sys
-import urllib.request
 import uuid
 
 dict_dir_name = "ispell-pl-20021127"
@@ -14,84 +13,8 @@ class CwGen:
     def __init__(self):
         """Class initialization"""
 
+        self.e2cw = e2cw.Ebook2Cw(os.path.dirname(sys.argv[0]))
         self.dictionary_list = []
-
-    def _md5(self, file_path):
-        """Calculate MD5 hash out of provided file
-
-        Args:
-            file_path (str): Path to the file to calculate MD5 out of
-
-        Returns:
-            string: A string containing HEX representation of calculated MD5 hash
-        """
-        hash_md5 = hashlib.md5()
-        with open(file_path, "rb") as f:
-            for data_chunk in iter(lambda: f.read(4096), b""):
-                hash_md5.update(data_chunk)
-
-        return hash_md5.hexdigest()
-
-    def _get_file_from_web(self, file_url, file_path):
-        """Downloads specified file from provided URL and store in selected location.
-            In order to keep data integrity file after downloading is saved with
-            suffix and later on is renamed to the original name assuring already
-            existing file gets deleted.
-
-        Args:
-            file_url  (str): URL of the file to download
-            file_path (str): Path to the resulting file (incl. file name)
-
-        Returns:
-            None
-        """
-
-        TEMP_FILE_SUFFIX = '_tmp'
-
-        # make sure directories are there
-        file_directory = os.path.dirname(file_path)
-        if not os.path.exists(file_directory):
-            os.makedirs(file_directory)
-
-        # download the file with temporary suffix
-        temp_file_path = file_path + TEMP_FILE_SUFFIX
-        urllib.request.urlretrieve(file_url, temp_file_path)
-
-        # verify if another copy already exist
-        if os.path.exists(file_path):
-            os.remove(file_path)
-
-        # rename downloaded file
-        os.rename(temp_file_path, file_path)
-
-    def _verify_file_md5(self, file_path, md5_file_path):
-        """Verifies if file's calculated MD5 hash match the one stored in other file.
-            File with pre-calculated MD5 hashes should contain
-            a row with hash and file name separated by space.
-
-        Args:
-            file_path     (str): Path to the file being verified
-            md5_file_path (str): Path to the file containing pre-calculated MD5 hashes
-
-        Returns:
-            bool: True when MD5 verification succeeded, False otherwise
-
-        """
-
-        is_verified = False
-
-        if os.path.exists(file_path) and os.path.exists(md5_file_path):
-            file_name = os.path.basename(file_path)
-
-            with open(os.path.normpath(md5_file_path), mode="r") as md5_file:
-                for line in md5_file:
-                    split_data = line.strip().split(None, 1)
-
-                    if split_data[1] == file_name and split_data[0] == self._md5(file_path):
-                        is_verified = True
-                        break
-
-        return is_verified
 
     def _get_words_stat(self, words_dictionary):
         """Generate a statistics on a dictionary data.
@@ -298,45 +221,7 @@ class CwGen:
         Returns:
             bool: True when executable was downloaded, False otherwise
         """
-
-        if_downloaded_ok = False
-
-        BASE_URL = 'https://fkurz.net/ham/ebook2cw/'
-        EXECUTABLE_BASE_NAME = 'ebook2cw'
-        HASH_FILE_NAME = 'md5sums-bin.txt'
-
-        current_os = platform.system()
-        if current_os == 'Windows' or current_os == 'Linux':
-            cwgen_path = os.path.dirname(sys.argv[0])
-            executable_local_base_path = os.path.normpath(os.path.join(
-                cwgen_path, 'ebook2cw'))
-
-            # prepare files urls/paths
-            executable_name = EXECUTABLE_BASE_NAME
-            if current_os == 'Windows':
-                executable_name += '.exe'
-            executable_url = BASE_URL + executable_name
-            executable_local_path = os.path.normpath(os.path.join(
-                executable_local_base_path, executable_name))
-
-            hash_file_url = BASE_URL + HASH_FILE_NAME
-            hash_file_local_path = os.path.normpath(os.path.join(
-                executable_local_base_path, HASH_FILE_NAME))
-
-            # download files (executable + md5)
-            self._get_file_from_web(
-                executable_url, executable_local_path)
-            self._get_file_from_web(
-                hash_file_url, hash_file_local_path)
-
-            # verify executable integrity
-            if self._verify_file_md5(
-                    executable_local_path, hash_file_local_path):
-                if_downloaded_ok = True
-            else:
-                os.remove(executable_local_path)
-
-        return if_downloaded_ok
+        return self.e2cw.get_executable()
 
 
 def main():
---
+++ helpers.py
@@ -0,0 +1,53 @@
+import os
+import hashlib
+import urllib.request
+
+
+def get_file_from_web(file_url, file_path):
+    """Downloads specified file from provided URL and store in selected location.
+        In order to keep data integrity file after downloading is saved with
+        suffix and later on is renamed to the original name assuring already
+        existing file gets deleted.
+
+    Args:
+        file_url  (str): URL of the file to download
+        file_path (str): Path to the resulting file (incl. file name)
+
+    Returns:
+        None
+    """
+
+    TEMP_FILE_SUFFIX = '_tmp'
+
+    # make sure directories are there
+    file_directory = os.path.dirname(file_path)
+    if not os.path.exists(file_directory):
+        os.makedirs(file_directory)
+
+    # download the file with temporary suffix
+    temp_file_path = file_path + TEMP_FILE_SUFFIX
+    urllib.request.urlretrieve(file_url, temp_file_path)
+
+    # verify if another copy already exist
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+    # rename downloaded file
+    os.rename(temp_file_path, file_path)
+
+
+def md5(file_path):
+    """Calculate MD5 hash out of provided file
+
+    Args:
+        file_path (str): Path to the file to calculate MD5 out of
+
+    Returns:
+        string: A string containing HEX representation of calculated MD5 hash
+    """
+    hash_md5 = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for data_chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(data_chunk)
+
+    return hash_md5.hexdigest()
